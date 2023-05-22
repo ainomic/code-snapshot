23de9c8d8a713b000fb31e2d0382ad45d2964851
Ainomic Dev <contact@ainomic.in>
2023-05-22 14:55:38 +0530

    Fix publish package

--- code_snapshot/__init__.py
+++ code_snapshot/__init__.py
@@ -1,3 +1,6 @@
 """code_snapshot - A Python library for interacting with the Showcode api."""
+from __future__ import absolute_import
+
+
+__version__ = "develop"
 __all__ = ['code_snapshot', 'models']
-__version__ = '0.3.0'
